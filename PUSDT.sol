d9f9b571b13b28382de88bc1698102a0cdf4f8e9
nickwildunique <82363696+nickwildunique@users.noreply.github.com>
2021-05-27 12:16:25 +0800

    Update PUSDT.sol

--- PUSDT.sol
+++ PUSDT.sol
@@ -16,48 +16,38 @@ contract PUSDT {
     uint8  public decimals = 6;
 
     uint256 public totalPUSDT = 0;
-    uint256 public totalUSDT = 0;
-    uint256 public totalInterest = 0;
-    uint256 public subTotalInterest = 0;
     uint256 public usedUSDT = 0;
     address public investAddr;
+    address public managerAddr;
 
-    bool private reEntrancyMutex = false;//Mutexes that prevent reentrant attacks
-    bool private canDeposit = true;//Allow to deposit.
+    bool public reEntrancyMutex = false;//Mutexes that prevent reentrant attacks
+    bool public canDeposit = true;//Allow to deposit.
     IERC20 usdt;
 
     event  Approval(address indexed src, address indexed guy, uint256 wad);
     event  Transfer(address indexed src, address indexed dst, uint256 wad);
     event  Deposit(address indexed dst, uint256 wad);
     event  Refund(address indexed src, uint256 principle);
-    event  AddInterest(address indexed src, uint256 interest);
-    event  SubInterest(address indexed src, uint256 interest);
-    event  Withdrawal(address indexed src, uint256 wad, uint256 ethNeed);
+    event  Withdrawal(address indexed src, uint256 wad);
     event  Invest(address indexed src, uint256 wad);
     event  ChangeIvAddr(address indexed src, address indexed newAddr);
+    event  ChangeMngAddr(address indexed src, address indexed newAddr);
     event  ChangeDeposit(address indexed src, bool canDeposit);
 
     mapping (address => uint256)                       public  balanceOf;
     mapping (address => mapping (address => uint256))  public  allowance;
 
-    constructor(address _investAddr,IERC20 _usdt) public {
+    constructor(address _investAddr,address _managerAddr,IERC20 _usdt) public {
         investAddr = _investAddr;
+        managerAddr = _managerAddr;
         usdt = _usdt;
     }
 
     function deposit(uint256 wad) public {
         require(canDeposit);
-        if (totalUSDT > 0 && totalPUSDT > 0 && totalUSDT != totalPUSDT) {
-            balanceOf[msg.sender] = wad.mul(totalPUSDT).div(totalUSDT).add(balanceOf[msg.sender]);
-            totalPUSDT = wad.mul(totalPUSDT).div(totalUSDT).add(totalPUSDT);
-            totalUSDT = totalUSDT.add(wad);
-            emit Transfer(address(0), msg.sender, wad.mul(totalPUSDT).div(totalUSDT));
-        } else {
-            balanceOf[msg.sender] = balanceOf[msg.sender].add(wad);
-            totalPUSDT = totalPUSDT.add(wad);
-            totalUSDT = totalUSDT.add(wad);
-            emit Transfer(address(0), msg.sender, wad);
-        }
+        balanceOf[msg.sender] = balanceOf[msg.sender].add(wad);
+        totalPUSDT = totalPUSDT.add(wad);
+        emit Transfer(address(0), msg.sender, wad);
         usdt.transferFrom(msg.sender, address(this), wad);
         emit Deposit(msg.sender, wad);
     }
@@ -66,37 +56,20 @@ contract PUSDT {
         usdt.transferFrom(msg.sender, address(this), principle);
         emit Refund(msg.sender, principle);
     }
-    function addInterest(uint256 interest) public {
-        totalUSDT = totalUSDT.add(interest);
-        totalInterest = totalInterest.add(interest);
-        usdt.transferFrom(msg.sender, address(this), interest);
-        emit AddInterest(msg.sender, interest);
-    }
-    function subInterest(uint256 interest) public {
-        require(msg.sender == investAddr, "Only investAddr can reduce interest.");
-        totalUSDT = totalUSDT.sub(interest);
-        subTotalInterest = subTotalInterest.add(interest);
-        usdt.transfer(msg.sender, interest);
-        emit SubInterest(msg.sender, interest);
-    }
     function withdraw(uint256 wad) public {
         require(!reEntrancyMutex);
-        uint256 assetNeed = 0;
-        if (totalPUSDT > 0) assetNeed = wad.mul(totalUSDT).div(totalPUSDT);
         balanceOf[msg.sender] = balanceOf[msg.sender].sub(wad);
         totalPUSDT = totalPUSDT.sub(wad);
-        totalUSDT  = totalUSDT.sub(assetNeed);
 
         reEntrancyMutex = true;
-        usdt.transfer(msg.sender, assetNeed);
+        usdt.transfer(msg.sender, wad);
         emit Transfer(msg.sender, address(0), wad);
-        emit Withdrawal(msg.sender, wad, assetNeed);
+        emit Withdrawal(msg.sender, wad);
         reEntrancyMutex = false;
     }
     function invest(uint256 wad) public {
-        require(msg.sender == investAddr, "Only investAddr can invest.");
         usedUSDT = usedUSDT.add(wad);
-        usdt.transfer(msg.sender, wad);
+        usdt.transfer(investAddr, wad);
         emit Invest(msg.sender, wad);
     }
     function changeIvAddr(address newAddr) public {
@@ -104,8 +77,13 @@ contract PUSDT {
         investAddr = newAddr;
         emit ChangeIvAddr(msg.sender, newAddr);
     }
+    function changeMngAddr(address newAddr) public {
+        require(msg.sender == managerAddr, "Only managerAddr can change manager Address.");
+        managerAddr = newAddr;
+        emit ChangeMngAddr(msg.sender, newAddr);
+    }
     function changeDeposit(bool _canDeposit) public {
-        require(msg.sender == investAddr, "Only investAddr can change Deposit State.");
+        require(msg.sender == managerAddr, "Only managerAddr can change Deposit State.");
         canDeposit = _canDeposit;
         emit ChangeDeposit(msg.sender, _canDeposit);
     }
