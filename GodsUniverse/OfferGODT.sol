bf73a35adac8e70aae65ea58e959d37ff4b37517
nickwildunique <82363696+nickwildunique@users.noreply.github.com>
2021-07-13 19:55:27 +0800

    Update OfferGODT.sol

--- GodsUniverse/OfferGODT.sol
+++ GodsUniverse/OfferGODT.sol
@@ -54,25 +54,33 @@ interface IERC20 {
 
 abstract contract Ownable {
     address private _owner;
+    address public pendingOwner;
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
     constructor() public {
         _setOwner(msg.sender);
     }
-    function owner() public view virtual returns (address) {
+    function owner() public view returns (address) {
         return _owner;
     }
     modifier onlyOwner() {
         require(owner() == msg.sender, "Ownable: caller is not the owner");
         _;
     }
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        _setOwner(newOwner);
+    modifier onlyPendingOwner() {
+        require(pendingOwner == msg.sender, "Ownable: caller is not the pendingOwner");
+        _;
+    }
+    function transferOwnership(address newOwner) public onlyOwner {
+        pendingOwner = newOwner;
     }
     function _setOwner(address newOwner) private {
         address oldOwner = _owner;
         _owner = newOwner;
         emit OwnershipTransferred(oldOwner, newOwner);
+    }
+    function claimOwnership() public onlyPendingOwner {
+        _setOwner(pendingOwner);
+        pendingOwner = address(0);
     }
 }
 
